6053cead0dd7e5b4479771e70b0ba84eb2abb1aa
John Louis De Regla <115703434+Jilofish@users.noreply.github.com>
2024-08-08 14:01:33 +0800

    Updated the badge categorization colors

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/DWOW.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/DWOW.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DWOW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ac9be79f6524029633087b2b6579f948e73017d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3abf956bca29269f79920316cc97b9fe5670df84")]
 [assembly: System.Reflection.AssemblyProductAttribute("DWOW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DWOW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
